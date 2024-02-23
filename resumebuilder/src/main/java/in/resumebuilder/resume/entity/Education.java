ee6129f1fad0a2a1998d84f54e4da8b7d2ebb437
gitrajrana <com.java.raj@gmail.com>
2024-02-24 00:24:00 +0530

    entity created

---
+++ resumebuilder/src/main/java/in/resumebuilder/resume/entity/Education.java
@@ -0,0 +1,26 @@
+package in.resumebuilder.resume.entity;
+
+public class Education {
+
+	private String schoolName;
+	private int yearBetween;
+	public String getSchoolName() {
+		return schoolName;
+	}
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	public int getYearBetween() {
+		return yearBetween;
+	}
+	public void setYearBetween(int yearBetween) {
+		this.yearBetween = yearBetween;
+	}
+	public double getMarks() {
+		return marks;
+	}
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
+	private double marks;
+}
